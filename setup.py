784ad5a9f7fdfc7e31347dd1e25abf98ce34e139
ratan221 <ratankumargupta685@gmail.com>
2025-07-27 22:21:33 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="GenerativeAI",
+    version="0.0.0",
+    author="Ratan Kumar Gupta",
+    author_email="ratankumargupta685@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+
+
+)
+
+
